526e4ec2c9f18d1f19eb59816c6b80e61ba33279
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-07-01 22:52:07 +0330

    marker bind popup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const map = L.map('map').setView([29.59012601341688, 52.585314720098346] , 10)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png' , {
-    maxZoom: 19,
+    zoom:12 ,
+    minZoom : 5  ,
+    maxZoom: 13,
 
 }).addTo(map)
+let marker = L.marker([29.594902646072292, 52.56883522873247] , ).addTo(map);
+marker.bindPopup(`
+    <h1  style="color : green ; ">این یک بیمه میباشد </h1>
+    `)
