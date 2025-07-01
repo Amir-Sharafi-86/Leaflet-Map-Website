4ed4c9f44fffb4684b5a83b9be00f5a37cbc93d3
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-07-01 23:43:56 +0330

    the test get position shops and show in maps

--- js/main.js
+++ js/main.js
@@ -5,7 +5,28 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png' , {
     maxZoom: 13,
 
 }).addTo(map)
-let marker = L.marker([29.594902646072292, 52.56883522873247] , ).addTo(map);
-marker.bindPopup(`
-    <h1  style="color : green ; ">این یک بیمه میباشد </h1>
-    `)
+
+
+const shops = [
+  {
+      position: [29.6015588126909, 52.562343568182996],
+      name: "فروشگاه مرکزی",
+      description: "شعبه اصلی",
+      img:'https://static.vecteezy.com/system/resources/previews/021/495/592/non_2x/3d-shop-icon-for-sale-and-decoration-png.png'
+  },
+  {
+      position: [29.59782151068345, 52.56165988039249],
+      name: "فروشگاه شمال",
+      description: "شعبه شمال",
+      img:'https://img.icons8.com/?size=50&id=343&format=png'
+  }
+];
+
+const storeIcon = L.icon({
+  iconUrl :'https://img.icons8.com/?size=48&id=8chNl15hy6jY&format=png',
+  iconSize : [20 ,20]
+})
+
+const resutlOutput  = shops.forEach(shop => {
+  L.marker(shop.position , {icon : storeIcon}).bindPopup(shop.description).addTo(map)
+})
